f2bb008edea89cf61d3f064384cc97c5d28a9465
giansoniaputra <giansonia555@gmail.com>
2023-06-28 15:52:57 +0700

    MEnyelesaikan Pelajaran Insert

---
+++ index.php
@@ -0,0 +1,2 @@
+<?php 
+header("Location: view/list_tugas.php");
--- view/list_tugas.php
+++ view/list_tugas.php
@@ -1,38 +1,60 @@
 <?php
 include '../model/tugas_harian.php';
-$data = select("SELECT * FROM tugas_harian where matkul = 'Kalkulus'");
+$data = select("SELECT * FROM tugas_harian");
+$data2 = select("SELECT * FROM tugas_mingguan");
 ?>
-<!DOCTYPE html>
-<html lang="en">
+<!-- Header -->
+<?php include '../layout/header.php'; ?>
+<!-- Header -->
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-
-<body>
-    <table border="1" cellspacing="0" cellpadding="2">
-        <thead>
+<h1>List Tugas</h1>
+<a href="tambah_tugas.php" class="btn btn-primary">Tambah Tugas</a>
+<!-- Table Tugas Harian -->
+<table class="table table-striped" id="dataTablesHarian">
+    <thead>
+        <tr>
+            <td>No</td>
+            <td>Mata Kuliah</td>
+            <td>Tugas</td>
+            <td>Deadline</td>
+        </tr>
+    </thead>
+    <tbody>
+        <?php $no = 1; ?>
+        <?php foreach ($data as $row) : ?>
             <tr>
-                <td>No</td>
-                <td>Mata Kuliah</td>
-                <td>Tugas</td>
-                <td>Deadline</td>
+                <td><?= $no++; ?></td>
+                <td><?= $row["matkul"]; ?></td>
+                <td><?= $row["tugas"]; ?></td>
+                <td><?= date("l, d-M-Y", strtotime($row["deadline"])); ?></td>
             </tr>
-        </thead>
-        <tbody>
-            <?php $no = 1; ?>
-            <?php foreach ($data as $row) : ?>
-                <tr>
-                    <td><?= $no++; ?></td>
-                    <td><?= $row["matkul"]; ?></td>
-                    <td><?= $row["tugas"]; ?></td>
-                    <td><?= date("l, d-M-Y", strtotime($row["deadline"])); ?></td>
-                </tr>
-            <?php endforeach; ?>
-        </tbody>
-    </table>
-</body>
+        <?php endforeach; ?>
+    </tbody>
+</table>
+<br><br>
+<!-- table tugas mingguan -->
+<table class="table table-striped">
+    <thead>
+        <tr>
+            <td>No</td>
+            <td>Mata Kuliah</td>
+            <td>Tugas</td>
+            <td>Deadline</td>
+        </tr>
+    </thead>
+    <tbody>
+        <?php $no = 1; ?>
+        <?php foreach ($data2 as $row) : ?>
+            <tr>
+                <td><?= $no++; ?></td>
+                <td><?= $row["matkul"]; ?></td>
+                <td><?= $row["tugas"]; ?></td>
+                <td><?= date("l, d-M-Y", strtotime($row["deadline"])); ?></td>
+            </tr>
+        <?php endforeach; ?>
+    </tbody>
+</table>
 
-</html>
+<!-- Footer -->
+<?php include '../layout/footer.php'; ?>
+<!-- Footer -->
